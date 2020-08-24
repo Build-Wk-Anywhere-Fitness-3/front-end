bdc61bf5c85c017ae5253dadf752e778cb17f875
amichalo421 <andre.michalowski@gmail.com>
2020-08-24 10:44:56 -0400

    Add basic component structure/folders

--- anywhere-fitness-front-end/src/App.js
+++ anywhere-fitness-front-end/src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/AAAReusable/Header';
+import Logo from './Components/BBBFrontPage/Logo';
+import SignInForm from './Components/BBBFrontPage/SignInForm';
+import Footer from './Components/AAAReusable/Footer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header />
+        {/* <Logo />
+        <SignInForm />
+        <Footer /> */}
       </header>
     </div>
   );
---
+++ anywhere-fitness-front-end/src/Components/AAAReusable/Header.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { Route, NavLink } from "react-router-dom";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+
+
+function Header (props) {
+ 
+
+  return (
+    <div>
+      <h1>Test</h1>
+    </div>
+  );
+}
+export default Header;
