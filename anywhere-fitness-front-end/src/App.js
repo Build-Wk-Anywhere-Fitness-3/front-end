4e31732ade003c8b6457cb7a9be60d4767233361
amichalo421 <andre.michalowski@gmail.com>
2020-08-24 13:01:46 -0400

    Styling added and basic components

--- anywhere-fitness-front-end/src/App.js
+++ anywhere-fitness-front-end/src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import './App.css';
+
+import { Route } from 'react-router-dom';
+
 import Header from './Components/AAAReusable/Header';
 import Logo from './Components/BBBFrontPage/Logo';
 import SignInForm from './Components/BBBFrontPage/SignInForm';
 import Footer from './Components/AAAReusable/Footer';
 
+import ClientDash from './Components/CCCSecondaryPages/Dashboard/ClientDash';
+import InstructorDash from './Components/CCCSecondaryPages/Dashboard/InstructorDash';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
         <Header />
-        {/* <Logo />
-        <SignInForm />
-        <Footer /> */}
-      </header>
+          <div className="App">
+            <Route exact path="/" components={{logo: Logo, signInForm: SignInForm}} />
+            <Route path="client-dash" component={ClientDash} />
+            <Route path="instructor-dash" component={InstructorDash} />
+            <Logo />
+            <SignInForm />
+            <Footer />
+          </div>
+        <Footer />
     </div>
   );
 }
--- anywhere-fitness-front-end/src/Components/AAAReusable/Header.js
+++ anywhere-fitness-front-end/src/Components/AAAReusable/Header.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-import { Route, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import {
   Collapse,
   Navbar,
   NavbarToggler,
   NavbarBrand,
   Nav,
+  NavLink,
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
@@ -14,11 +15,46 @@ import {
 
 
 function Header (props) {
- 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
 
   return (
     <div>
-      <h1>Test</h1>
+      <Navbar color="light" light expand="md" className="Navbar" >
+        <NavbarBrand href="/">| &ensp;A N Y W H E R E &ensp; F I T N E S S{" "}</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <h2>
+              <NavLink href="localhost:3000/" className="navLink">Home</NavLink>
+            </h2>
+            <h2>
+              <NavLink href="https://github.com/Build-Wk-Anywhere-Fitness-3" className="navLink">GitHub</NavLink>
+            </h2>
+
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Sign 
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+
+          </Nav>
+          {/* <h3>Simple Text</h3> */}
+        </Collapse>
+      </Navbar>
     </div>
   );
 }
